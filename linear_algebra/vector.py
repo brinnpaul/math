409407772437124d55caa8c51169593f47b2e24e
Brinn <brinn.riordan@gmail.com>
2017-03-09 17:33:47 -0500

    update vector

--- linear_algebra/vector.py
+++ linear_algebra/vector.py
@@ -120,9 +120,6 @@ class Vector(object):
 
     def project(self, v1, v2):
 
-
-
-
     @staticmethod
     def test_vector(v):
         try:
@@ -138,34 +135,3 @@ class Vector(object):
                 raise ValueError
         except:
             raise ValueError('Vectors must be of same dimension')
-
-
-
-
-v1 = Vector([-7.579,-7.88])
-v2 = Vector([22.737, 23.64])
-
-v3 = Vector([-2.029, 9.97, 4.172])
-v4 = Vector([-9.231, -6.639, -7.245])
-
-v5 = Vector([-2.328, -7.284, -1.214])
-v6 = Vector([-1.821, 1.072, -2.94])
-
-v7 = Vector([2.118, 4.827])
-v8 = Vector([0, 0])
-
-print ('V1 on V2')
-print (v1.check_if_parallel(v2))
-print (v1.check_if_orthagonal(v2))
-
-print ('V3 on V4')
-print (v3.check_if_parallel(v4))
-print (v3.check_if_orthagonal(v4))
-
-print ('V5 on V6')
-print (v5.check_if_parallel(v6))
-print (v5.check_if_orthagonal(v6))
-
-print ('V7 on V8')
-print (v7.check_if_parallel(v8))
-print (v7.check_if_orthagonal(v8))
